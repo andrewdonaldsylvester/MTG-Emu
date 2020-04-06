a3cd937af13355e7e8a6ba30ee92ede896e39e33
Funny Dog <35876206+andrewdonaldsylvester@users.noreply.github.com>
2020-04-06 09:58:53 -0700

    Update card_types.py
    Formatting and removed useless parameters

--- card_types.py
+++ card_types.py
@@ -1,10 +1,13 @@
 import requests
 
+
 def fetch_card_data(card_name):
     result = requests.get("https://api.scryfall.com/cards/named?exact={}".format(card_name.replace(' ', '+')))
     return result.json()
 
+
 print(fetch_card_data("Wrenn and Six"))
+
 
 class Card(object):
     def __init__(self, name):
@@ -25,38 +28,45 @@ class Card(object):
 
 
 class Creature(Card):
-    def __init__(self, name): # add abilities as a list or maybe just haste; add summoning sickness as a trait
+    def __init__(self, name):  # add abilities as a list or maybe just haste; add summoning sickness as a trait
         super(Creature, self).__init__(name)
         self.power = self.card_data['power']
         self.toughness = self.card_data['toughness']
+
 
 class Artifact(Card):
     def __init__(self, name):
         super(Artifact, self).__init__(name)
 
+
 class Enchantment(Card):
     def __init__(self, name):
         super(Enchantment, self).__init__(name)
+
 
 class Land(Card):
     def __init__(self, name):
         super(Land, self).__init__(name)
 
+
 class Planeswalker(Card):
-    def __init__(self, name): # put loyalty abilities as a dictionary with cost:ability
+    def __init__(self, name):  # put loyalty abilities as a dictionary with cost:ability
         super(Planeswalker, self).__init__(name)
         self.starting_loyalty = self.card_data['loyalty']
         self.loyalty = self.starting_loyalty
         # self.loyalty_abilities = loyalty_abilities
         # self.static_abilities = static_abilities
 
+
 class Instant(Card):
-    def __init__(self, name, mana_cost, power, toughness):
+    def __init__(self, name):
         super(Instant, self).__init__(name)
 
+
 class Sorcery(Card):
-    def __init__(self, name, mana_cost, power, toughness):
+    def __init__(self, name):
         super(Sorcery, self).__init__(name)
+
 
 ramos = Creature('Ramos, Dragon Engine')
 
